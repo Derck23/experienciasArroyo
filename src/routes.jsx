9742a021e52f03935e584d9b7ca35c77bf4d70b2
Derck23 <eramireznieves25@gmail.com>
2025-10-05 14:50:58 -0600

    correccion h

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Experiencia from "./pages/Experiencia/Experiencia";
 
 
