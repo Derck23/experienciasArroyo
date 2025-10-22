274b2869548fb58e698bc77e9d316b0098cbf943
Tu Nombre <2022371192@uteq.edu.mx>
2025-10-21 18:06:14 -0600

    empaquetamiento de aplicacion con capacitor

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import 'antd/dist/reset.css'
 //import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+// Registrar el service worker
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
